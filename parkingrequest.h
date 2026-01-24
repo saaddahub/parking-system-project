7ef76e142dd771529d7607568e785d6a6b9b6d5f
saaddahub <saadsalam659@gmail.com>
2026-01-24 14:59:00 +0500

    added time and penalty

--- parkingrequest.h
+++ parkingrequest.h
@@ -1,33 +1,19 @@
-#ifndef PARKINGREQUEST_H
-#define PARKINGREQUEST_H
-
+#ifndef parkingrequest_h
+#define parkingrequest_h
 #include "vehicle.h"
-#include <ctime>
 #include <string>
-using namespace std;
-
-// The PDF specifically asks for these states
-enum RequestStatus
-{
-    REQUESTED,
-    ALLOCATED,
-    OCCUPIED,
-    CANCELLED,
-    RELEASED
-};
 
 class ParkingRequest
 {
 public:
-    int requestID;
     Vehicle *vehicle;
-    RequestStatus status;
-    time_t requestTime; // PDF asks for "Request time" [cite: 41]
+
+    int status;
+    int startTime;
+    int endTime;
+    double penaltyCost;
 
     ParkingRequest(Vehicle *v);
-
-    void updateStatus(RequestStatus newStatus);
-    string getStatusString();
+    void updateStatus(int newStatus);
 };
-
 #endif
